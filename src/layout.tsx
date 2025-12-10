d74591b86e403f7d239a8d3c1671462add839f33
rishi.rathore <rishi.rathore@liseinfotech.com>
2025-12-10 14:33:40 +0530

    added changes

--- src/layout.tsx
+++ src/layout.tsx
@@ -4,7 +4,7 @@ import Appnavbar from '@/components/ui/app-navbar'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className='flex justify-between lg:w-11/12 md:w-11/12 sm:w-full mx-auto h-screen'>
+        <div className='flex justify-between lg:w-10/12 md:w-10/12 sm:w-full mx-auto h-screen'>
             <div className=''>
                 <DashboardSidebar />
             </div>
